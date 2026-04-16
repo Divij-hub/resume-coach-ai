287258644958ac9f3e16982a57deb82b1d3777e5
divij-hub <divijdevkota99@gmail.com>
2026-04-16 16:03:36 -0400

    fix: correct cloudfront restrictions syntax

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -49,6 +49,16 @@ resource "aws_cloudfront_distribution" "main" {
     response_page_path = "/index.html"
   }
 
-  restrictions { geo_restriction { restriction_type = "none" } }
-  viewer_certificate { cloudfront_default_certificate = true }
+ # ... other configuration above ...
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
 }
